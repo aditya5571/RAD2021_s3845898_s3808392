a37f7753eabfbef7a2f28e07f2aeb1a8b9cc7cf3
Ubuntu <ubuntu@ip-172-31-12-22.us-east-2.compute.internal>
2021-05-23 12:30:48 +0000

    Seeds replenished

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,3 @@
 Product.destroy_all
 products = Product.create([ {company: "1", title: "title1", size: "s", price: "10", imageString: "shop_08.jpg", category: "winter", description: "this is product 1"}, 
                             {company: "2", title: "title2", size: "s", price: "20", imageString: "shop_09.jpg", category: "winter", description: "this is product 2"}])
-
-ProductVariant.destroy_all
-ProductVariant.create([{product_id: 6, colour: "blue", size: "l", quantity: 5},
-                        {product_id: 7, colour: "red", size: "s", quantity: 3},
-                        {product_id: 7, colour: "red", size: "xl", quantity: 6},
-                        {product_id: 7, colour: "blue", size: "xl", quantity: 1}])
