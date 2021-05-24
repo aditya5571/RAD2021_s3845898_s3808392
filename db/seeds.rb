87d2d5148e71a41110ef3eb8e287d5c6657efc14
Dhruv Bachani <s3808392@student.rmit.edu.au>
2021-05-24 03:14:31 +0000

    product variants added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,37 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.destroy_all
-products = Product.create([ {id: 2, company: "1", title: "Yoga outfit", popularity: 4, price: "40.00", imageString: "shop_08.jpg", category: "women", description: "this is product 1"}, 
-                            {id: 3, company: "2", title: "Top", popularity: 10, price: "35.55", imageString: "shop_09.jpg", category: "women", description: "this is product 2"},
-                            {id: 4, company: "3", title: "Jeans", popularity: 13, price: "59.99", imageString: "shop_10.jpg", category: "men", description: "jeans is product 3"},
-                            {id: 5, company: "4", title: "Jacket", popularity: 19, price: "129.99", imageString: "shop_11.jpg", category: "men", description: "jacket is product 4"},
-                            {id: 6, company: "5", title: "Skirt", popularity: 7, price: "16.99", imageString: "shop_12.jpg", category: "kids", description: "skirt is product 5"}])
+products = Product.create([ {id: 1, company: "1", title: "Yoga outfit", popularity: 4, price: "40.00", imageString: "shop_08.jpg", category: "women", description: "this is product 1"}, 
+                            {id: 2, company: "2", title: "Top", popularity: 10, price: "35.55", imageString: "shop_09.jpg", category: "women", description: "this is product 2"},
+                            {id: 3, company: "3", title: "Jeans", popularity: 13, price: "59.99", imageString: "shop_10.jpg", category: "men", description: "jeans is product 3"},
+                            {id: 4, company: "4", title: "Jacket", popularity: 19, price: "129.99", imageString: "shop_11.jpg", category: "men", description: "jacket is product 4"},
+                            {id: 5, company: "5", title: "Skirt", popularity: 7, price: "16.99", imageString: "shop_12.jpg", category: "kids", description: "skirt is product 5"}])
 
 ProductVariant.destroy_all
-product_variants = ProductVariant.create([  {product_id: 2, colour: "Red", size: "s", quantity: 5},
-                                            {product_id: 2, colour: "Blue", size: "s", quantity: 8},
-                                            {product_id: 2, colour: "Black", size: "xl", quantity: 4},
-                                            {product_id: 2, colour: "Green", size: "l", quantity: 9},
-                                            {product_id: 3, colour: "Red", size: "s", quantity: 1},
-                                            {product_id: 3, colour: "Red", size: "m", quantity: 3},
-                                            {product_id: 3, colour: "Red", size: "l", quantity: 4},
-                                            {product_id: 3, colour: "Green", size: "xl", quantity: 13},
-                                            {product_id: 3, colour: "Black", size: "xl", quantity: 2}])
+product_variants = ProductVariant.create([  {product_id: 1, colour: "Red", size: "s", quantity: 5},
+                                            {product_id: 1, colour: "Blue", size: "s", quantity: 8},
+                                            {product_id: 1, colour: "Black", size: "xl", quantity: 4},
+                                            {product_id: 1, colour: "Green", size: "l", quantity: 9},
+                                            {product_id: 2, colour: "Red", size: "s", quantity: 1},
+                                            {product_id: 2, colour: "Red", size: "m", quantity: 3},
+                                            {product_id: 2, colour: "Red", size: "l", quantity: 4},
+                                            {product_id: 2, colour: "Green", size: "xl", quantity: 13},
+                                            {product_id: 2, colour: "Black", size: "xl", quantity: 2},
+                                            {product_id: 3, colour: "Red", size: "s", quantity: 5},
+                                            {product_id: 3, colour: "Blue", size: "s", quantity: 8},
+                                            {product_id: 3, colour: "Black", size: "xl", quantity: 4},
+                                            {product_id: 3, colour: "Green", size: "l", quantity: 9},
+                                            {product_id: 4, colour: "Red", size: "s", quantity: 1},
+                                            {product_id: 4, colour: "Red", size: "m", quantity: 3},
+                                            {product_id: 4, colour: "Red", size: "l", quantity: 4},
+                                            {product_id: 4, colour: "Green", size: "xl", quantity: 13},
+                                            {product_id: 4, colour: "Black", size: "xl", quantity: 2},
+                                            {product_id: 5, colour: "Black", size: "xl", quantity: 4},
+                                            {product_id: 5, colour: "Green", size: "l", quantity: 9},
+                                            {product_id: 5, colour: "Red", size: "s", quantity: 1},
+                                            {product_id: 5, colour: "Blue", size: "s", quantity: 8},
+                                            {product_id: 5, colour: "Black", size: "xl", quantity: 4},
+                                            {product_id: 5, colour: "Green", size: "l", quantity: 9},
+                                            {product_id: 5, colour: "Red", size: "s", quantity: 1}])
+                                            
+    
